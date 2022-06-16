8c839b010a3177e138a5583ce28fba19b5a44579
russell davidson <silencer_65@hotmail.com>
2022-06-16 17:23:55 +0100

    #100daysofcode - implement pickups

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private GameObject scoreline;
+    private Text scoredisplay;
+    private int collected;
+
+    private void Awake()
+    {
+        scoredisplay = scoreline.GetComponent<Text>();
+    }
+
+    
     void Start()
+    {        
+        collected = 0;
+        scoredisplay.text = $"Collected: {collected} ";
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D coll)
     {
-        
+        if (coll.gameObject.tag == "Collectable")
+        {
+            //add to pickup counter etc
+            collected += 1;
+            Destroy(coll.gameObject);
+            Debug.Log($"You got a {coll.gameObject.name} nice one!");
+            scoredisplay.text = $"Collected: {collected} ";
+        }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //This serves as a skeleton for the player class.
+    
+    int lives = 0; //how many times player can die.
+       
+    int hearts = 0; //how many times player can be hit
+
+    int score = 0; //will be linked to enemy kills
+
+    //TODO implement "weapons" or rather the spray gun thingy.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
